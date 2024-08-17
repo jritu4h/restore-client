a584e2135c835a19c23210d1e84a82aef2c5b1b5
HABIBA JANNAT RITU <jritu5820@gmail.com>
2024-08-17 18:46:30 +0600

    added sex commit

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -14,7 +14,7 @@ const Banner = () => {
       <h1 className="text-5xl font-bold"> Estore LTD</h1>
       <p className="py-4">
       Welcome to Estore LTD, your one-stop shop.
-                     Discover great deals on quality prod.
+                     Discover great deals on quality produ.
                     Experience shopping like never before.
       </p>
       <button className="btn bg-blue-500 hover:bg-blue-500 text-white">SHOP NOW</button>
